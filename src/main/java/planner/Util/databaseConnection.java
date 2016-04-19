325c006e55cada4af5729a636b71c2a0da82fc0b
linuxguy467 <dpplrdr435@gmail.com>
2016-04-19 11:39:33 -0400

    New JDBC driver added

--- src/main/java/planner/Util/databaseConnection.java
+++ src/main/java/planner/Util/databaseConnection.java
@@ -27,7 +27,7 @@ public class databaseConnection
     public static Connection connectToDatabase()
     {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage() + e.getCause());
         }
